7ff72a8ed8471fcf8da89e6554eb08d2c7af07d3
Renan Eduardo F. da Silva <renaneduardo1@hotmail.com>
2023-03-21 16:41:03 -0300

    test AddToCartButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button/button';
+import { AddToCartButton } from './components/AddToCartButton/AddToCartButton';
+import { Button } from './components/Button/Button';
 import { defaultTheme } from './styles/themes/default';
 export function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
-			<h1>Hello world</h1>
 			<Button />
+			<AddToCartButton />
 		</ThemeProvider>
 	);
 }
