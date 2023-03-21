7d9eb2fe25808bc8925cd11c4b2f8936ca1c6322
Renan Eduardo F. da Silva <renaneduardo1@hotmail.com>
2023-03-21 10:36:56 -0300

    create button container to test colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { ThemeProvider } from 'styled-components';
+import { Button } from './components/Button/button';
+import { defaultTheme } from './styles/themes/default';
 export function App() {
-	return <h1>Hello world</h1>;
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<h1>Hello world</h1>
+			<Button />
+		</ThemeProvider>
+	);
 }
