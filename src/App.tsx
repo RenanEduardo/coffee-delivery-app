7ce26b5950a3e1c7d23d16ee27b8f741281ccd61
Renan Eduardo F. da Silva <renaneduardo1@hotmail.com>
2023-03-21 17:00:09 -0300

    create global style

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { ThemeProvider } from 'styled-components';
 import { AddToCartButton } from './components/AddToCartButton/AddToCartButton';
 import { Button } from './components/Button/Button';
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/GlobalStyle.styles';
 export function App() {
 	return (
-		<ThemeProvider theme={defaultTheme}>
-			<Button />
-			<AddToCartButton />
-		</ThemeProvider>
+		<>
+			<ThemeProvider theme={defaultTheme}>
+				<GlobalStyle />
+				<Button />
+				<AddToCartButton />
+			</ThemeProvider>
+		</>
 	);
 }
