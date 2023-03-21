c5803ea85f09fc7695865798497e2601bcfd343a
Renan Eduardo F. da Silva <renaneduardo1@hotmail.com>
2023-03-21 10:36:33 -0300

    add default theme colors

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,26 @@
+export const defaultTheme = {
+	//COLORS
+	colors: {
+		product: {
+			yellowDark: '#c47f17',
+			yellow: '#dbac2c',
+			yellowLight: '#f1e9c9',
+			purpleDark: '#4b2995',
+			purple: '#8047f8',
+			lightPurple: '#ebe5f9',
+		},
+
+		base: {
+			baseTitle: '#272221',
+			baseSubtitle: '#403937',
+			baseText: '#574f4d',
+			baseLabel: '#8d8686',
+			baseHover: '#d7d5d5',
+			baseButton: '#e6e5e5',
+			baseInput: '#ededed',
+			baseCard: '#f3f2f2',
+			background: '#fafafa',
+			white: '#ffffff',
+		},
+	},
+};
